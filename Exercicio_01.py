96a1c3a35af38c2d1f54b952393f0a068b20cab1
João Victor Nunes de Moura <jotaven.moura@gmail.com>
2022-03-22 13:07:18 +0000

    Update Exercicio_01.py

--- Exercicio_01.py
+++ Exercicio_01.py
@@ -1,5 +1,5 @@
-letra = str(input('letra: '))
-if letra.lower() in "aeiou":
-    print('É uma vogal')
+letra = str(input('letra: ')).strip().lower()
+if letra in "aeiou":
+    print('É uma Vogal')
 else:
     print('É uma Consoante')

3e383a335c40604550982c2c44398f1222b744d3
João Victor Nunes de Moura <jotaven.moura@gmail.com>
2022-03-22 13:09:48 +0000

    Update Exercicio_01.py

--- Exercicio_01.py
+++ Exercicio_01.py
@@ -1,3 +1,6 @@
+'''
+Escreva um programa que recebe uma letra do usuário e diz se a letra foi uma vogal.
+'''
 letra = str(input('letra: ')).strip().lower()
 if letra in "aeiou":
     print('É uma Vogal')

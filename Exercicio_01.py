de95dc2d6f46f07df7e0a5de48ee3d2c79eb044e
João Victor Nunes de Moura <jotaven.moura@gmail.com>
2022-03-23 11:37:28 +0000

    Delete Exercicio_01.py

--- Exercicio_01.py
+++
@@ -1,8 +0,0 @@
-'''
-Escreva um programa que recebe uma letra do usuário e diz se a letra foi uma vogal.
-'''
-letra = str(input('letra: ')).strip().lower()
-if letra in "aeiou":
-    print('É uma Vogal')
-else:
-    print('É uma Consoante')

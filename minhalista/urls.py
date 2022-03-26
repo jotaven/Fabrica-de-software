d616530bf45784637ff0bd919f5bddd04790239a
João Victor Nunes de Moura <jotaven.moura@gmail.com>
2022-03-26 17:00:18 -0300

    adicionar CSS

--- minhalista/urls.py
+++ minhalista/urls.py
@@ -4,7 +4,7 @@ from .views import viewApagar, viewCriar, viewDetalhe, viewPrincipal
 
 urlpatterns = [
        path("principal/", viewPrincipal, name = "principal"),
+       path('criar-item', viewCriar, name='criar'),
        path('list-item/<int:id>/', viewDetalhe, name='detalhe'),
        path('apagar/<int:id>/', viewApagar, name='apague'),
-       path('criar-item', viewCriar, name='criar')
 ]
